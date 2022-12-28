33e70052208881313babc82b3d827e876d046cf5
Diego Doná <diedona@gmail.com>
2022-12-28 08:55:52 -0300

    add design ef core

--- DDona.SimpleAuth.Domain/Entities/Base/BaseEntity.cs
+++ DDona.SimpleAuth.Domain/Entities/Base/BaseEntity.cs
@@ -2,10 +2,10 @@
 {
     public abstract class BaseEntity
     {
+        protected BaseEntity() { }
+
         public Guid Id { get; set; }
         public DateTime CreatedUtc { get; set; }
         public DateTime? UpdatedUtc { get; set;}
-
-        protected BaseEntity() { }
     }
 }
