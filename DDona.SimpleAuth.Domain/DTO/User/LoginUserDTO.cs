d5206af55dac2d6c0092e6cb7eaa71e64e4e7451
Diego Doná <diedona@gmail.com>
2022-12-28 21:28:35 -0300

    login, create user

---
+++ DDona.SimpleAuth.Domain/DTO/User/LoginUserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace DDona.SimpleAuth.Domain.DTO.User
+{
+    public class LoginUserDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
